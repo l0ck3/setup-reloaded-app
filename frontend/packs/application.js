fe35697c23e5f9420e081814ac234730720326bb
Olivier Nottebart <olivier.nottebart@gmail.com>
2019-06-26 20:17:32 +0200

    Front front

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -2,10 +2,21 @@
 
 import 'bootstrap';
 import 'stylesheets/application';
-// import { pickingGender } from './gender-pick';
 
 document.addEventListener('DOMContentLoaded', () => {
-   const picking = document.querySelectorAll('.gender-pick')
-   picking.forEach((picked) => picked.addEventListener('click', (pick) => { console.log(pick.currentTarget.dataset.gender) }))
+  const selectGender = (pick) => {
+    const selected = pick.currentTarget.dataset.gender
+    pick.currentTarget.classList.toggle('selected-gender')
+    const radioGender = document.querySelector(`#student_gender_${selected}`)
+    if (radioGender) { radioGender.checked = true }
+    const osPicked = document.querySelector(`#student_os_${selected}`)
+    if (osPicked) { osPicked.checked = true }
+
+  }
+  const picking = document.querySelectorAll('.gender-pick')
+  picking.forEach((picked) => picked.addEventListener('click', (pick) => { selectGender(pick) } ))
+
+  // const fieldSet = document.querySelector('.student_gender')
+  // fieldSet.classList.add('hidden')
 })
 
